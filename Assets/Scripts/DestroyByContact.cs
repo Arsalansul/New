659e1b9b14dffbebdc440e03045d5e2757d5a1da
arsalansul <arsalansul@gmail.com>
2016-08-09 01:05:23 +0700

    fadfasd

---
+++ Assets/Scripts/DestroyByContact.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DestroyByContact : MonoBehaviour 
+{
+	public GameObject explosion;
+	public GameObject playerExplosion;
+	public int scoreValue;
+	private GameControlle gameController;
+
+	void Start ()
+	{
+		GameObject gameControllerObject = GameObject.FindWithTag ("GameController");
+		if (gameControllerObject != null )
+		{
+			gameController = gameControllerObject.GetComponent <GameControlle>();
+		}
+		if (gameControllerObject == null) 
+		{
+			Debug.Log ("Can not find Game Controller");
+		}
+	}
+
+	void OnTriggerEnter (Collider other)
+	{	
+
+		if (other.tag == "Boundary")
+			return;
+		Destroy (other.gameObject);
+		Destroy (gameObject);
+
+		if (other.tag == "Player") 
+		{
+			Instantiate (playerExplosion, other.transform.position, other.transform.rotation);
+			gameController.GameOver ();
+		}
+		
+		Instantiate(explosion, transform.position, transform.rotation);
+
+		gameController.AddScore (scoreValue);
+	}
+
+}
